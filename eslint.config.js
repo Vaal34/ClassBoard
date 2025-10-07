a9db7cb0592dbb6d2b2c8877ab4dc61d27a1bc27
Vaal34 <5619@holbertonstudents.com>
2025-10-07 19:44:02 +0200

    add pretttier

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import prettier from 'eslint-config-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -12,6 +13,7 @@ export default defineConfig([
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      prettier, // Désactive les règles ESLint qui entrent en conflit avec Prettier
     ],
     languageOptions: {
       ecmaVersion: 2020,
